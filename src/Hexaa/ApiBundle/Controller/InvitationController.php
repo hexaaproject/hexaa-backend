fb7b9f594298a4804ff5f24689711cdfa98ff5fc
Soltész Balázs <solazs@sztaki.hu>
2014-06-23 14:57:35 +0200

    Invitation controller kezdemény

---
+++ src/Hexaa/ApiBundle/Controller/InvitationController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Hexaa\ApiBundle\Controller;
+
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use FOS\RestBundle\Routing\ClassResourceInterface;
+
+use FOS\RestBundle\Util\Codes;
+
+use FOS\RestBundle\Controller\Annotations;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Request\ParamFetcherInterface;
+use FOS\RestBundle\View\RouteRedirectView;
+
+use FOS\RestBundle\View\View;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+
+use Hexaa\StorageBundle\Form\InvitationType;
+use Hexaa\StorageBundle\Entity\Invitation;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Rest controller for HEXAA
+ *
+ * @package Hexaa\ApiBundle\Controller
+ * @author Soltész Balázs <solazs@sztaki.hu>
+ */
+class InvitationController extends FOSRestController {
+    
+    
+    /**
+     * get invitation details
+     *
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   statusCodes = {
+     *     200 = "Returned when successful",
+     *     401 = "Returned when token is expired",
+     *     403 = "Returned when not permitted to query",
+     *     404 = "Returned when resource is not found"
+     *   },
+     * requirements ={
+     *      {"name"="id", "dataType"="integer", "required"=true, "requirement"="\d+", "description"="invitation id"},
+     *      {"name"="_format", "requirement"="xml|json", "description"="response format"}
+     *  }
+     * )
+     *
+     * 
+     * @Annotations\View()
+     *
+     * @param Request               $request      the request object
+     * @param ParamFetcherInterface $paramFetcher param fetcher attribute specification
+     *
+     * @return Invitation
+     */
+    public function getInvitationAction(Request $request, ParamFetcherInterface $paramFetcher, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $i = $em->getRepository('HexaaStorageBundle:Invitation')->find($id);
+        if (!$i) throw new HttpException(404, 'Invitation not found.');
+        return $i;
+    }
+    
+    
+    /**
+     * send new invitation
+     *
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   statusCodes = {
+     *     200 = "Returned when successful",
+     *     401 = "Returned when token is expired",
+     *     403 = "Returned when not permitted to query",
+     *     404 = "Returned when resource is not found"
+     *   },
+     * requirements ={
+     *      {"name"="_format", "requirement"="xml|json", "description"="response format"}
+     *  }
+     * )
+     *
+     * 
+     * @Annotations\View()
+     *
+     * @param Request               $request      the request object
+     * @param ParamFetcherInterface $paramFetcher param fetcher attribute specification
+     *
+     * @return Invitation
+     */
+    public function postInvitationAction(Request $request, ParamFetcherInterface $paramFetcher, $id)
+    {
+        throw new HttpException(400, "not implemented, yet!");
+    }
+    
+}
