817d73ad1e12d68e0c3f93d5c2fbf8ec7fe4fec7
Soltész Balázs <solazs@sztaki.hu>
2014-07-03 14:41:52 +0200

    formok legenerálva az meghívásokhoz

---
+++ src/Hexaa/StorageBundle/Form/EmailInvitationType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Hexaa\StorageBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class EmailInvitationType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('uuid')
+            ->add('status')
+            ->add('counter')
+            ->add('createdAt')
+            ->add('acceptAt')
+            ->add('lastReinviteAt')
+            ->add('role')
+            ->add('organization')
+            ->add('service')
+            ->add('inviter')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Hexaa\StorageBundle\Entity\EmailInvitation'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'hexaa_storagebundle_emailinvitation';
+    }
+}
---
+++ src/Hexaa/StorageBundle/Form/UrlInvitationType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Hexaa\StorageBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UrlInvitationType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('emails')
+            ->add('url')
+            ->add('status')
+            ->add('landingUrl')
+            ->add('doRedirect')
+            ->add('asManager')
+            ->add('description')
+            ->add('counter')
+            ->add('createdAt')
+            ->add('startDate')
+            ->add('endDate')
+            ->add('limit')
+            ->add('role')
+            ->add('organization')
+            ->add('inviter')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Hexaa\StorageBundle\Entity\UrlInvitation'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'hexaa_storagebundle_urlinvitation';
+    }
+}
