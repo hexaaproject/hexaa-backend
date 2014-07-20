07edff295578ae4bf3c9b460a77d5facdf8f38b0
Soltész Balázs <soltesz.balazs.91@gmail.com>
2014-07-20 18:12:05 +0200

    Cache-be az osztályokat is, a gyorsabb hívásokért.

--- app/autoload.php
+++ app/autoload.php
@@ -1,8 +1,13 @@
 <?php
 
+require __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/ClassLoader/ApcClassLoader.php';
+use Symfony\Component\ClassLoader\ApcClassLoader;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
+
+
 $loader = require __DIR__.'/../vendor/autoload.php';
+$loader = new ApcClassLoader('apc.loader', $loader);
 $loader->add('sspmod_saml_',__DIR__.'/../vendor/simplesamlphp/modules/saml/lib/');
 $loader->add('SimpleSAML_',__DIR__.'/../vendor/simplesamlphp/lib/');
 $loader->add('Auth_',__DIR__.'/../vendor/simplesamlphp/vendor/openid/php-openid/');
