e96bfe3f70b26d1dfa480a9324cffeedc1fd19d4
Soltész Balázs <solazs@sztaki.hu>
2014-07-21 17:42:37 +0200

    még egy kis SAML gyomlálás

--- app/autoload.php
+++ app/autoload.php
@@ -6,10 +6,12 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 
 
 $loader = require __DIR__.'/../vendor/autoload.php';
+/*
 $loader->add('sspmod_saml_',__DIR__.'/../vendor/simplesamlphp/modules/saml/lib/');
 $loader->add('SimpleSAML_',__DIR__.'/../vendor/simplesamlphp/lib/');
 $loader->add('Auth_',__DIR__.'/../vendor/simplesamlphp/vendor/openid/php-openid/');
 $loader->add('SAML2_',__DIR__.'/../vendor/simplesamlphp/lib/');
+ */
 
 // intl
 if (!function_exists('intl_get_error_code')) {
