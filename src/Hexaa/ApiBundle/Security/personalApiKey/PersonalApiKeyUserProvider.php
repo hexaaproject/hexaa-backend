74fac805b77d0dc3203f7f5589c7251ef91e423c
Soltész Balázs <solazs@sztaki.hu>
2014-08-07 17:25:20 +0200

    reset 1 hour token expiration upon token use

--- src/Hexaa/ApiBundle/Security/personalApiKey/PersonalApiKeyUserProvider.php
+++ src/Hexaa/ApiBundle/Security/personalApiKey/PersonalApiKeyUserProvider.php
@@ -12,11 +12,13 @@ use Monolog\Logger;
 
 class PersonalApiKeyUserProvider implements UserProviderInterface {
     protected $loginlog;
+    protected $modlog;
     protected $logLbl;
 
-    public function __construct($container, Logger $loginlog) {
+    public function __construct($container, Logger $loginlog, Logger $modlog) {
         $this->container = $container;
         $this->loginlog = $loginlog;
+        $this->loginlog = $modlog;
         $this->logLbl = "[personalApiKeyAuth] ";
     }
 
@@ -24,7 +26,7 @@ class PersonalApiKeyUserProvider implements UserProviderInterface {
         $em = $this->container->get("doctrine")->getManager();
         $p = $em->getRepository("HexaaStorageBundle:Principal")->findOneByToken($apiKey);
         if (!($p instanceof Principal)) {
-            $this->loginlog->error($this->logLbl."token not found in database");
+            $this->loginlog->error($this->logLbl."Token not found in database");
             throw new HttpException(403, 'Invalid token!');
         } else {
             $date = new \DateTime();
@@ -32,10 +34,16 @@ class PersonalApiKeyUserProvider implements UserProviderInterface {
             $tokenExp = $p->getTokenExpire();
             $diff = $tokenExp->diff($date, true);
             if (($date < $tokenExp) && ($diff->h > 1)) {
-                $this->loginlog->error($this->logLbl."token expired.");
+                $this->loginlog->error($this->logLbl."Token expired for principal with id=".$p->getId());
                 throw new HttpException(401, 'Token expired');
             } else {
-                $this->loginlog->info($this->logLbl."user ".$p->getFedid()." successfully authenticated");
+                $this->loginlog->info($this->logLbl."User ".$p->getFedid()." successfully authenticated");
+                $date->modify('+1 hour');
+                $p->setTokenExpire($date);
+                $em->persist($p);
+                $em->flush();
+                $this->loginlog->info($this->logLbl."Token expiration reset for user id=".$p->getId());
+                $this->modlog->info($this->logLbl."Token expiration reset for user id=".$p->getId());
                 $username = $p->getFedid();
                 return $username;
             }
