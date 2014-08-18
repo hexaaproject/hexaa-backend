9e49d26e78a4e3205b8ca26d07f588c83a019d75
Soltész Balázs <solazs@sztaki.hu>
2014-08-18 14:32:54 +0200

    minor changes

--- src/Hexaa/StorageBundle/Security/masterSecret/MasterSecretUserProvider.php
+++ src/Hexaa/StorageBundle/Security/masterSecret/MasterSecretUserProvider.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Hexaa\StorageBundle\Security\masterSecret;
 
 use Symfony\Component\Security\Core\User\UserProviderInterface;
@@ -8,54 +9,49 @@ use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Monolog\Logger;
 
+class MasterSecretUserProvider implements UserProviderInterface {
 
-class MasterSecretUserProvider implements UserProviderInterface
-{
     private $secret;
     protected $loginlog;
     protected $logLbl;
-    
+
     public function __construct($secret, Logger $loginlog) {
         $this->secret = $secret;
         $this->loginlog = $loginlog;
         $this->logLbl = "[masterSecretAuth] ";
     }
-    public function getUsernameForApiKey($apiKey)
-    {
+
+    public function getUsernameForApiKey($apiKey) {
         // Generate hashes to compare with
         $time = new \DateTime();
         date_timezone_set($time, new \DateTimeZone('UTC'));
         $stamp1 = $time->format('Y-m-d H:i');
-        $hash1 = hash('sha256',$this->secret.$stamp1);
+        $hash1 = hash('sha256', $this->secret . $stamp1);
         $time->sub(new \DateInterval('PT1M'));
-        $stamp1 = $time->format('Y-m-d H:i');
-        $hash2 = hash('sha256',$this->secret.$stamp1);
-        
-        // Actually compare
-        if ($apiKey == $hash1 || $apiKey == $hash2)
-        {
-            $this->loginlog->info($this->logLbl."master secret authentication successful");
-	  $username = "master"; // some dummy username
-	  return $username;
+        $stamp2 = $time->format('Y-m-d H:i');
+        $hash2 = hash('sha256', $this->secret . $stamp2);
+
+        // Compare, and authenticate or deny entry
+        if ($apiKey == $hash1 || $apiKey == $hash2) {
+            $this->loginlog->info($this->logLbl . "master secret authentication successful");
+            $username = "master"; // some dummy username
+            return $username;
         } else {
-            $this->loginlog->error($this->logLbl."API key is invalid or expired");
-	  throw new HttpException(403, "Invalid api key.");
+            $this->loginlog->error($this->logLbl . "API key is invalid or expired");
+            throw new HttpException(403, "Invalid api key.");
         }
     }
 
-    public function loadUserByUsername($username)
-    {
+    public function loadUserByUsername($username) {
         return new User(
-            $username,
-            null,
-            // the roles for the user - you may choose to determine
-            // these dynamically somehow based on the user
-            array('ROLE_API')
+                $username, null,
+                // the roles for the user - you may choose to determine
+                // these dynamically somehow based on the user
+                array('ROLE_API')
         );
     }
 
-    public function refreshUser(UserInterface $user)
-    {
+    public function refreshUser(UserInterface $user) {
         // this is used for storing authentication in the session
         // but in this example, the token is sent in each request,
         // so authentication can be stateless. Throwing this exception
@@ -63,8 +59,8 @@ class MasterSecretUserProvider implements UserProviderInterface
         throw new UnsupportedUserException();
     }
 
-    public function supportsClass($class)
-    {
+    public function supportsClass($class) {
         return 'Symfony\Component\Security\Core\User\User' === $class;
     }
-} 
+
+}
