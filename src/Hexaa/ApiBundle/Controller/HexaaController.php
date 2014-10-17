d339e72cc3b5da5ca63f20000eb6ccec7894dfba
Soltész Balázs <solazs@sztaki.hu>
2014-10-17 15:48:56 +0200

    Use bootstrap file in app.php

--- src/Hexaa/ApiBundle/Controller/HexaaController.php
+++ src/Hexaa/ApiBundle/Controller/HexaaController.php
@@ -49,6 +49,6 @@ class HexaaController extends FOSRestController {
      * @return array
      */
     public function getPropertiesAction(Request $request, ParamFetcherInterface $paramFetcher) {
-        return array("version" => "0.15.0", "entitlement_base" => $this->container->getParameter("hexaa_entitlement_uri_prefix"));
+        return array("version" => "0.15.1", "entitlement_base" => $this->container->getParameter("hexaa_entitlement_uri_prefix"));
     }
 }
