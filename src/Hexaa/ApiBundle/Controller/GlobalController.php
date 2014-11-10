e6e278416aba47dbfa8dbfd506f9340970846338
Soltész Balázs <solazs@sztaki.hu>
2014-11-10 10:46:12 +0100

    version

--- src/Hexaa/ApiBundle/Controller/GlobalController.php
+++ src/Hexaa/ApiBundle/Controller/GlobalController.php
@@ -37,37 +37,6 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class GlobalController extends FOSRestController implements PersonalAuthenticatedController{
     
-
-    /**
-     * get HEXAA backend properties
-     *
-     *
-     * @ApiDoc(
-     *   section = "Other",
-     *   resource = true,
-     *   statusCodes = {
-     *     200 = "Returned when successful",
-     *     401 = "Returned when token is expired or invalid",
-     *     403 = "Returned when not permitted to query",
-     *     404 = "Returned when service is not found"
-     *   },
-     *   requirements ={
-     *      {"name"="_format", "requirement"="xml|json", "description"="response format"}
-     *   }
-     * )
-     *
-     * 
-     * @Annotations\View()
-     *
-     * @param Request               $request      the request object
-     * @param ParamFetcherInterface $paramFetcher param fetcher service
-     *
-     * @return array
-     */
-    public function getPropertiesAction(Request $request, ParamFetcherInterface $paramFetcher) {
-        return array("version" => "0.18.1", "entitlement_base" => $this->container->getParameter("hexaa_entitlement_uri_prefix"));
-    }
-    
     /**
      * List all existing and enabled service entityIDs from HEXAA config
      *
@@ -108,6 +77,36 @@ class GlobalController extends FOSRestController implements PersonalAuthenticate
 
         $retarr = array_slice($this->container->getParameter('hexaa_service_entityids'), $paramFetcher->get('offset'), $paramFetcher->get('limit'));
         return $retarr;
+    }    
+
+    /**
+     * get HEXAA backend properties
+     *
+     *
+     * @ApiDoc(
+     *   section = "Other",
+     *   resource = true,
+     *   statusCodes = {
+     *     200 = "Returned when successful",
+     *     401 = "Returned when token is expired or invalid",
+     *     403 = "Returned when not permitted to query",
+     *     404 = "Returned when service is not found"
+     *   },
+     *   requirements ={
+     *      {"name"="_format", "requirement"="xml|json", "description"="response format"}
+     *   }
+     * )
+     *
+     * 
+     * @Annotations\View()
+     *
+     * @param Request               $request      the request object
+     * @param ParamFetcherInterface $paramFetcher param fetcher service
+     *
+     * @return array
+     */
+    public function getPropertiesAction(Request $request, ParamFetcherInterface $paramFetcher) {
+        return array("version" => "0.18.2", "entitlement_base" => $this->container->getParameter("hexaa_entitlement_uri_prefix"));
     }
     
 }
