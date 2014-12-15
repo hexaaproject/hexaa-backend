b5d9a7ab4fae845975c6567ed3950105066e5adb
Soltész Balázs <solazs@sztaki.hu>
2014-12-15 14:02:04 +0100

    add post-update script to create database, update schema and clear production cache.

---
+++ post-update.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Creating database if necessary..."
+
+php app/console doctrine:database:create | grep -q "database exists"
+
+echo "done."
+
+echo "Dumping schema changes:"
+php app/console doctrine:schema:update --dump-sql
+
+echo
+read -p "Update database schema? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    php app/console doctrine:schema:update --force
+fi
+
+read -p "Clear production cache? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  php app/console cache:clear --env=prod
+fi
