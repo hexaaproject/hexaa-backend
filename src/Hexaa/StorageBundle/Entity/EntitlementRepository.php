6583fbae7a6d42fb7f665850e1dae647ca4b9ea8
Soltész Balázs <solazs@sztaki.hu>
2015-01-29 16:14:17 +0100

    don't list entitlements of pending entitlementpack in organization

--- src/Hexaa/StorageBundle/Entity/EntitlementRepository.php
+++ src/Hexaa/StorageBundle/Entity/EntitlementRepository.php
@@ -20,6 +20,7 @@ class EntitlementRepository extends EntityRepository {
                 ->innerJoin('oep.entitlementPack', 'ep')
                 ->where('oep.organization = :o')
                 ->andWhere('e MEMBER OF ep.entitlements')
+                ->andWhere("oep.status == 'accepted'")
                 ->setFirstResult($offset)
                 ->setMaxResults($limit)
                 ->setParameters(array('o' => $o))
