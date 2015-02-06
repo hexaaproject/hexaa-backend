e797a3f36748b03534aa23a2c7a7a78663ffddc5
Soltész Balázs <solazs@sztaki.hu>
2015-02-06 16:49:10 +0100

    fix AttributeValue validators

--- src/Hexaa/ApiBundle/Validator/Constraints/AttributeSpec4UserValidator.php
+++ src/Hexaa/ApiBundle/Validator/Constraints/AttributeSpec4UserValidator.php
@@ -2,13 +2,14 @@
 
 namespace Hexaa\ApiBundle\Validator\Constraints;
 
+use Hexaa\StorageBundle\Entity\AttributeSpec;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
 class AttributeSpec4UserValidator extends ConstraintValidator {
 
     public function validate($value, Constraint $constraint) {
-        if (!$value) {
+        if ( ! $value instanceof AttributeSpec ) {
             $this->context->addViolation($constraint->notFoundMessage);
             $this->context->addViolationAt("attribute_spec", $constraint->notFoundMessage);
         }
