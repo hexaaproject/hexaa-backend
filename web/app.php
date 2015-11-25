d7a67e18b28d730a7a4629d65b17d4130cd1ab6a
solazs <soltesz.balazs.91@gmail.com>
2015-11-25 15:21:10 +0100

    switch over to xcache, because opcache has no 'userspace' cahcing.

--- web/app.php
+++ web/app.php
@@ -1,19 +1,15 @@
 <?php
-use Symfony\Component\ClassLoader\ApcClassLoader;
+
+use Symfony\Component\ClassLoader\XcacheClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
 //Use bootstrap file to speed up the bootstrapping process
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 //$loader = require_once __DIR__.'/../app/autoload.php';
 
-// Use APC for autoloading to improve performance.
-// Change 'sf2' to a unique prefix in order to prevent cache key conflicts
-// with other applications also using APC.
-
-/* DISABLED because we now use opcache where available.
-$loader = new ApcClassLoader('sf2', $loader);
+//Use xcache instead of opcache, because opcode cache is just not enough
+$loader = new XcacheClassLoader('sf2', $loader);
 $loader->register(true);
-*/
 
 require_once __DIR__.'/../app/AppKernel.php';
 require_once __DIR__.'/../app/AppCache.php';
