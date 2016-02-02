b5a9380db53d440b3dded717ae51d5bf1dd69c67
solazs <solazs@sztaki.hu>
2016-02-02 16:24:12 +0100

    remove cache magic from app.php

--- web/app.php
+++ web/app.php
@@ -1,22 +1,11 @@
 <?php
 
-require_once(__DIR__ . '/../vendor/symfony/symfony/src/Symfony/Component/ClassLoader/XcacheClassLoader.php');
-
-use Symfony\Component\ClassLoader\XcacheClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
 //Use bootstrap file to speed up the bootstrapping process
 //$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
 $loader = require_once __DIR__.'/../app/autoload.php';
-
-// sha1(__FILE__) generates an XCache namespace prefix
-$cachedLoader = new XcacheClassLoader(sha1(__FILE__), $loader);
-
-// register the cached class loader
-$cachedLoader->register(true);
-
-$loader->unregister();
 
 require_once __DIR__.'/../app/AppKernel.php';
 require_once __DIR__.'/../app/AppCache.php';
