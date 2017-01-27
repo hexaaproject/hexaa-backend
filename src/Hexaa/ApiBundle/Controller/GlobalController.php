4abedeed2d4780133e416f93489b8c228c584e8c
Soltész Balázs <solazs@sztaki.hu>
2017-01-27 13:00:58 +0100

    v0.30.4

--- src/Hexaa/ApiBundle/Controller/GlobalController.php
+++ src/Hexaa/ApiBundle/Controller/GlobalController.php
@@ -418,7 +418,7 @@ class GlobalController extends HexaaController implements PersonalAuthenticatedC
         $this->accesslog->info($loglbl . "Called by " . $p->getFedid());
 
         return array(
-          "version"                       => "0.30.3",
+          "version"                       => "0.30.4",
           "entitlement_base"              => $this->container->getParameter("hexaa_entitlement_uri_prefix"),
           "public_attribute_spec_enabled" => $this->container->getParameter("hexaa_public_attribute_spec_enabled")
         );
