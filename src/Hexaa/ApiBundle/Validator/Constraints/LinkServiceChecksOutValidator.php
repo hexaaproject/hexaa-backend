651265b9043cf1ffc632791b9924ea95db2aae58
Soltész Balázs <solazs@sztaki.hu>
2017-02-03 16:49:17 +0100

    various fixes across the board

--- src/Hexaa/ApiBundle/Validator/Constraints/LinkServiceChecksOutValidator.php
+++ src/Hexaa/ApiBundle/Validator/Constraints/LinkServiceChecksOutValidator.php
@@ -27,18 +27,42 @@ class LinkServiceChecksOutValidator extends ConstraintValidator
         foreach ($l->getEntitlements() as $entitlement) {
             if ($entitlement->getService()->getId() != $sid) {
                 $this->context->buildViolation($constraint->entitlementNotForServiceMessage)
+                  ->setParameters(
+                    array(
+                      '%eid%' => $entitlement->getId(),
+                      '%sid%' => $sid,
+                    )
+                  )
                   ->addViolation();
                 $this->context->buildViolation($constraint->entitlementNotForServiceMessage)
                   ->atPath("entitlements")
+                  ->setParameters(
+                    array(
+                      '%eid%' => $entitlement->getId(),
+                      '%sid%' => $sid,
+                    )
+                  )
                   ->addViolation();
             }
         }
         foreach ($l->getEntitlementPacks() as $entitlementPack) {
             if ($entitlementPack->getService()->getId() != $sid) {
                 $this->context->buildViolation($constraint->entitlementPackNotForServiceMessage)
+                  ->setParameters(
+                    array(
+                      '%epid%' => $entitlementPack->getId(),
+                      '%sid%'  => $sid,
+                    )
+                  )
                   ->addViolation();
                 $this->context->buildViolation($constraint->entitlementPackNotForServiceMessage)
                   ->atPath("entitlement_packs")
+                  ->setParameters(
+                    array(
+                      '%epid%' => $entitlementPack->getId(),
+                      '%sid%'  => $sid,
+                    )
+                  )
                   ->addViolation();
             }
         }
