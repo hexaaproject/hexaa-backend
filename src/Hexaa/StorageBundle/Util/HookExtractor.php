d79d9c8392d56bb3c70fe8675f3eeaee0b3b7791
Soltész Balázs <solazs@sztaki.hu>
2017-02-27 14:25:38 +0100

    hook_mods: lose the unnecessary attribute query

--- src/Hexaa/StorageBundle/Util/HookExtractor.php
+++ src/Hexaa/StorageBundle/Util/HookExtractor.php
@@ -105,10 +105,7 @@ class HookExtractor
           ->setParameter(':sids', $sids)
           ->getQuery()
           ->getResult();
-
-        $avps = array();
         $retarr = array();
-        $attrNames = array();
 
         /* @var $hook Hook */
         foreach ($hs as $hook) {
@@ -127,145 +124,16 @@ class HookExtractor
 
             $fedids = array_unique(array_merge($oldFedids, $newFedids));
 
-            /* @var $principals ArrayCollection */
-            $principals = $this->em->createQueryBuilder()
-              ->select('p')
-              ->from('HexaaStorageBundle:Principal', 'p')
-              ->where('p.fedid in (:fedids)')
-              ->setParameter(':fedids', $fedids)
-              ->getQuery()
-              ->getResult();
-
-            /* @var $p Principal */
-            foreach ($principals as $p) {
-                $attributes = array();
-
-                // Get Consent object, or create it if it doesn't exist
-                $c = $this->em->getRepository('HexaaStorageBundle:Consent')->findOneBy(
-                  array(
-                    "principal" => $p,
-                    "service"   => $s,
-                  )
-                );
-                if (!$c) {
-                    $c = new Consent();
-                    $c->setService($s);
-                    $c->setPrincipal($p);
-                    $this->em->persist($c);
-                }
-
-                // Get attribute spec - service connectors
-                $sass = $this->em->createQueryBuilder()
-                  ->select("sas")
-                  ->from('HexaaStorageBundle:ServiceAttributeSpec', 'sas')
-                  ->where("sas.service = :s")
-                  ->setParameters(array("s" => $s))
-                  ->getQuery()
-                  ->getResult();
-
-                //  Get the values by principal
-                /* @var $sas ServiceAttributeSpec */
-                foreach ($sass as $sas) {
-                    $releaseAttributeSpec = $c->hasEnabledAttributeSpecs($sas->getAttributeSpec());
-                    if ($this->hexaa_consent_module == false || $this->hexaa_consent_module == "false") {
-                        $releaseAttributeSpec = true;
-                    }
-                    if ($releaseAttributeSpec) {
-                        $tmps = $this->em->getRepository('HexaaStorageBundle:AttributeValuePrincipal')->findBy(
-                          array(
-                            "attributeSpec" => $sas->getAttributeSpec(),
-                            "principal"     => $p,
-                          )
-                        );
-                        /* @var $tmp AttributeValuePrincipal */
-                        foreach ($tmps as $tmp) {
-                            if ($tmp->hasService($s) || ($tmp->getServices()->count() == 0)) {
-                                $avps[] = $tmp;
-                            }
-                        }
+            foreach ($fedids as $fedid) {
+                if (!array_key_exists($s->getId(), $data)) {
+                    $hookStuff['content'][$fedid] = array();
+                } else {
+                    if (!array_key_exists($fedid, $data[$s->getId()])) {
+                        $hookStuff['content'][$fedid] = array();
+                    } else {
+                        $hookStuff['content'][$fedid] = $data[$s->getId()][$fedid];
                     }
                 }
-                // Place the attributes in the return array
-                /* @var $avp AttributeValuePrincipal */
-                foreach ($avps as $avp) {
-                    $attributes[$avp->getAttributeSpec()->getUri()] = array();
-                    if (!in_array($avp->getAttributeSpec()->getName(), $attrNames)) {
-                        $attrNames[] = $avp->getAttributeSpec()->getName();
-                    }
-                }
-
-                /* @var $avp AttributeValuePrincipal */
-                foreach ($avps as $avp) {
-                    if (!in_array($avp->getValue(), $attributes[$avp->getAttributeSpec()->getUri()])) {
-                        array_push($attributes[$avp->getAttributeSpec()->getUri()], $avp->getValue());
-                    }
-                }
-
-                // Get the values by organization
-                $avos = $this->em->getRepository('HexaaStorageBundle:AttributeValueOrganization')->findAll();
-                /* @var $avo \Hexaa\StorageBundle\Entity\AttributeValueOrganization */
-                foreach ($avos as $avo) {
-                    if ($avo->hasService($s) || ($avo->getServices()->count() == 0)) {
-                        if (!array_key_exists($avo->getAttributeSpec()->getUri(), $attributes)) {
-                            $attributes[$avo->getAttributeSpec()->getUri()] = array();
-                        }
-
-                        if (!in_array($avo->getAttributeSpec()->getName(), $attrNames)) {
-                            $attrNames[] = $avo->getAttributeSpec()->getName();
-                        }
-                        if (!in_array($avo->getValue(), $attributes[$avo->getAttributeSpec()->getUri()])) {
-                            array_push($attributes[$avo->getAttributeSpec()->getUri()], $avo->getValue());
-                        }
-                    }
-                }
-
-                // Check if we have consent to entitlement release
-                $releaseEntitlements = $c->getEnableEntitlements();
-                if ($this->hexaa_consent_module == false || $this->hexaa_consent_module == "false") {
-                    $releaseEntitlements = true;
-                }
-                if ($releaseEntitlements) {
-                    $es = $this->em->getRepository('HexaaStorageBundle:Entitlement')->findAllByPrincipalAndService($p, $s);
-
-                    if ((!isset($attributes['urn:oid:1.3.6.1.4.1.5923.1.1.1.7'])
-                        || !is_array($attributes['urn:oid:1.3.6.1.4.1.5923.1.1.1.7']))
-                      && count($es) > 0
-                    ) {
-                        $attributes['urn:oid:1.3.6.1.4.1.5923.1.1.1.7'] = array();
-                        $attrNames[] = 'eduPersonEntitlement';
-                    }
-                    foreach ($es as $e) {
-                        $attributes['urn:oid:1.3.6.1.4.1.5923.1.1.1.7'][] = $e->getUri();
-                    }
-                }
-
-                if (count($attributes) != 0) {
-                    $hookStuff['content'][$p->getFedid()] = $attributes;
-                }
-
-                $releasedAttributes = "";
-                foreach ($attrNames as $attrName) {
-                    $releasedAttributes = $releasedAttributes." ".$attrName.", ";
-                }
-                $releasedAttributes = substr($releasedAttributes, 0, strlen($releasedAttributes) - 2);
-                $this->releaseLog->info(
-                  "[attribute release] released attributes [".$releasedAttributes
-                  ." ] of user with fedid=".$p->getFedid()." to service with entityid=".$s->getEntityid()
-                );
-
-                //Create News object to notify the user
-                $n = new News();
-                $n->setPrincipal($p);
-                $n->setService($s);
-                $n->setTitle("Attribute release");
-                $n->setMessage(
-                  "We have released some attributes (".$releasedAttributes." ) of ".$n->getPrincipal()->getFedid(
-                  )." to service ".$s->getName()
-                );
-                $n->setTag("attribute_release");
-                $this->em->persist($n);
-                $this->em->flush();
-
             }
             $retarr[] = $hookStuff;
         }
